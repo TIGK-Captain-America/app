9cd9cb90390e7f484e44ee3ad3e78a8336c5bd3a
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-04-28 11:30:34 +0200

    tested with robot & start with send com

--- lib/src/views/home/viewModel.dart
+++ lib/src/views/home/viewModel.dart
@@ -11,16 +11,33 @@ class HomeViewModel extends ChangeNotifier {
   void init() async {
     List<BluetoothService> services = await _deviceService.discoverServices();
 
-    services.forEach((service) {
-      print("services ${service}");
+    services.forEach((service) async {
+      //print("service $service");
+      if (service.uuid == Guid("0000ffe1-0000-1000-8000-00805f9b34fb")) {
+        service.characteristics.forEach((c) async {        
+          if (c.uuid == Guid('0000ffe3-0000-1000-8000-00805f9b34fb')) {
+            print("props2 = ${c.properties}");
+            if (c.properties.write) {
+              try {
+                await c.write([49], withoutResponse: true);
+                await Future.delayed(const Duration(milliseconds: 100), () {});
+              } catch (e) {
+                print(e);
+              }
+            }
+          }
+      });
+      }
+      //print("services ${service}");
     });
 
+    /*
     var characteristics = services[0].characteristics;
     for (BluetoothCharacteristic c in characteristics) {
       print(c.uuid);
       List<int> value = await c.read();
       print("value $value");
-    }
+    }*/
   }
 
   Future disconnectDevice() async {
